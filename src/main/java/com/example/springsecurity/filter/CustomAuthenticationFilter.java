43cca67dd9685ef15255694488705c78e6ff02f8
itsbhupendra <bhupendrasingh12398@gmail.com>
2021-09-12 17:58:08 +0530

    SecurityConfig class created

---
+++ src/main/java/com/example/springsecurity/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.example.springsecurity.filter;
+
+public class CustomAuthenticationFilter {
+}
--- src/main/java/com/example/springsecurity/service/AppUserServiceImpl.java
+++ src/main/java/com/example/springsecurity/service/AppUserServiceImpl.java
@@ -6,15 +6,21 @@ import com.example.springsecurity.repo.RolesRepo;
 import com.example.springsecurity.repo.UserRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @Transactional
 @Slf4j
-public class AppUserServiceImpl implements AppuserService{
+public class AppUserServiceImpl implements AppuserService, UserDetailsService {
     private final UserRepo userRepo;
     private final RolesRepo rolesRepo;
 
@@ -22,6 +28,22 @@ public class AppUserServiceImpl implements AppuserService{
     public AppUserServiceImpl(UserRepo userRepo, RolesRepo rolesRepo) {
         this.userRepo = userRepo;
         this.rolesRepo = rolesRepo;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser= userRepo.findByUsername(username);
+        if(appUser==null){
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        }
+        else{
+            log.info("User found in the database :{}",username);
+        }
+        Collection<SimpleGrantedAuthority> authorities=new ArrayList<>();
+        appUser.getRoles().forEach(roles -> {
+            authorities.add(new SimpleGrantedAuthority(roles.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(appUser.getUsername(),appUser.getPassword(),authorities);
     }
 
     @Override
@@ -56,4 +78,6 @@ public class AppUserServiceImpl implements AppuserService{
         log.info("Fetching all users");
         return userRepo.findAll();
     }
+
+
 }
