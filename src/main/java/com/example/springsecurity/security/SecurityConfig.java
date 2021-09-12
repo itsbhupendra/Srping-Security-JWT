a4a68c855f555527f0a374771632758b2664e9fd
itsbhupendra <bhupendrasingh12398@gmail.com>
2021-09-12 21:00:26 +0530

    JWT completed

--- src/main/java/com/example/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import static org.springframework.http.HttpMethod.GET;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -30,10 +32,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-         http.csrf().disable();
+        CustomAuthenticationFilter customAuthenticationFilter=new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+        http.csrf().disable();
          http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-         http.authorizeRequests().anyRequest().permitAll();
-         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+         http.authorizeRequests().antMatchers("/api/login/**").permitAll();
+         http.authorizeRequests().antMatchers("/api/user/**").hasAnyAuthority("ROLE_USER");
+         http.authorizeRequests().antMatchers("/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+         http.authorizeRequests().antMatchers(GET,"/api/user/**").hasAnyAuthority("ROLE_USER");
+         http.authorizeRequests().anyRequest().authenticated();
+         http.addFilter(customAuthenticationFilter);
     }
 
     @Bean
